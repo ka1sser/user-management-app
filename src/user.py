2b25a83549debf8e51a9445c224687631dda7f8d
ka1sser <serenokaisser.dev@gmail.com>
2025-02-12 10:25:42 +0800

    Added user.py which contains the User Class

---
+++ src/user.py
@@ -0,0 +1,93 @@
+"""This module contains the User Class"""
+
+
+class User:
+    """This class handles all operations related to individual user"""
+
+    def __init__(
+        self,
+        user_name,
+        user_password,
+        user_id,
+        auth_algo,
+        priv_algo,
+        status,
+        permission,
+    ):
+
+        self.user_name = user_name
+        self.user_password = user_password
+        self.user_id = user_id
+        self.auth_algo = auth_algo
+        self.priv_algo = priv_algo
+        self.status = status
+        self.permission = permission
+
+    def change_password(self, new_password):
+        """
+        This function changes the user password
+
+        Args:
+            new_password (str): New password input by user
+
+        Returns:
+            self.user_password (str): Returns the new password
+        """
+        self.user_password = new_password
+
+        return self.user_password
+
+    def enable(self):
+        """
+        This function changes the user status to "Enabled".
+
+        Returns:
+            self.status (str): Returns "Enabled" as a status.
+        """
+        self.status = "Enabled"
+
+        return self.status
+
+    def disable(self):
+        """
+        This function changes the user status to "Disabled".
+
+        Returns:
+            self.status (str): Returns "Disabled" as a status.
+        """
+        self.status = "Disabled"
+
+        return self.status
+
+    def lock(self):
+        """
+        This function changes the user permission to False.
+
+        Returns:
+            self.permission (bool): Returns a boolean value False
+        """
+        self.permission = False
+
+        return self.permission
+
+    def unlock(self):
+        """
+        This function changes the user permission to True.
+
+        Returns:
+            self.permission (bool): Returns a boolean value True
+        """
+        self.permission = True
+
+        return self.permission
+
+    def reset_user_pw(self):
+        """
+        This function resets the user password to default value "123456"
+
+        Returns:
+            self.user_password: Returns the default password string "123456"
+        """
+        self.user_password = "123456"
+
+        return self.user_password
